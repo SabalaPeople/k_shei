49f62944d556638c20a020510cd5976285bb0554
sabalapeople <sabalard01@gmail.com>
2020-08-11 13:53:03 +0800

    test

--- jsx/index.jsx
+++ jsx/index.jsx
@@ -31,7 +31,7 @@ class HomePage extends React.Component {
 
 
 
-class RouterApp extends Component {
+class RouterApp extends React.Component {
     render() {
       return (
         <BrowserRouter>
