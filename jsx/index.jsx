0c6db2c09a0d56c785e2acb838733add6ace3f8a
sabalapeople <sabalard01@gmail.com>
2020-08-11 13:40:10 +0800

    init1

---
+++ jsx/index.jsx
@@ -0,0 +1,40 @@
+class Main extends React.Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+    }
+
+    
+
+    render(){
+        return(
+            <div >
+                
+                ABCD
+
+            </div>
+        );
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<Main/>, document.body);
